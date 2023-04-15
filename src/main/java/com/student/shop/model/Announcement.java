3af26b3921e6c74c56ca7cb6d0a6b6260d82e8f2
yikedu <duyikee@gmail.com>
2023-04-15 22:49:33 +0100

    Announcement

--- src/main/java/com/student/shop/model/Announcement.java
+++ src/main/java/com/student/shop/model/Announcement.java
@@ -9,8 +9,6 @@ import lombok.Setter;
 import javax.persistence.*;
 
 /**
- * 公告
- *
  * @author Yike Du
  * @email student@test.com
  * @date 2023-3-18
@@ -18,7 +16,7 @@ import javax.persistence.*;
 @Entity
 @Setter
 @Getter
-@Table(name = "t_announcement")
+@Table(name = "tbl_announcement")
 public class Announcement extends AbstractEntity {
 
     @Column(columnDefinition="VARCHAR(512) NOT NULL COMMENT '公告内容'")
